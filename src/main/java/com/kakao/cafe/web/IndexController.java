87fdffc8a574ef5a38bbb5f86ef23ec5fb8eff72
KTH-96 <taehyun960@gmail.com>
2022-05-18 15:16:15 +0900

    feat: Dto생성및 controller변경

--- src/main/java/com/kakao/cafe/web/IndexController.java
+++ src/main/java/com/kakao/cafe/web/IndexController.java
@@ -1,41 +1,18 @@
 package com.kakao.cafe.web;
 
-import com.kakao.cafe.domain.user.User;
-import com.kakao.cafe.service.PostsService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
+@Slf4j
+@RequestMapping("/")
 @Controller
 public class IndexController {
-    private static final Logger log = LoggerFactory.getLogger(IndexController.class);
-    private final PostsService postsService;
 
-    @Autowired
-    public IndexController(PostsService postsService) {
-        this.postsService = postsService;
-    }
-
-    @GetMapping("/")
-    public String index(Model model, HttpServletRequest request) {
-        log.info("인덱스 페이지 불러오기");
-        HttpSession session = request.getSession(false);
-        model.addAttribute("post", postsService.findPosts());
-
-        if (session == null) {
-            return "index";
-        }
-        User loginUser = (User) session.getAttribute("loginUser");
-        if (loginUser == null) {
-            return "index";
-        }
-        model.addAttribute("user", loginUser);
-        return "index";
-    }
+	@GetMapping
+	public String index() {
+		return "index";
+	}
 }
