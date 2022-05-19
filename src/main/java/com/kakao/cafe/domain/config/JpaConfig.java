b1f3331a28d58134d743d420039d254b2269cc71
KTH-96 <taehyun960@gmail.com>
2022-05-19 14:01:35 +0900

    feat: OAuth2적용해보기
    - member에 picture, role변수 추가

---
+++ src/main/java/com/kakao/cafe/domain/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.kakao.cafe.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+}
--- src/main/java/com/kakao/cafe/domain/member/Member.java
+++ src/main/java/com/kakao/cafe/domain/member/Member.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -31,13 +33,39 @@ public class Member extends BaseTimeEntity {
 	@Column(length = 100, nullable = false)
 	private String nickname;
 
+	@Column
+	private String picture;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Role role;
+
 	@OneToMany(mappedBy = "member")
 	private List<Post> postList = new ArrayList<>();
+
+	@Builder
+	public Member(String email, String nickname, String picture, Role role) {
+		this.email = email;
+		this.nickname = nickname;
+		this.picture = picture;
+		this.role = role;
+	}
 
 	@Builder
 	public Member(String email, String password, String nickname) {
 		this.email = email;
 		this.password = password;
 		this.nickname = nickname;
+	}
+
+
+
+	public Member oauthUpdate(String name, String picture) {
+		this.nickname = name;
+		return this;
+	}
+
+	public String getRoleKey() {
+		return this.role.getKey();
 	}
 }
